f300d9fa809f13aeb00081ca05193a704fa35aa9
A Z M Monir Hossain <monir@AZMs-MacBook-Pro.local>
2023-04-23 22:37:01 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAZC6Y5oGkvDOAST9Kt811JAlJqhPLXMBE",
+  authDomain: "the-news-dragon-client-b5551.firebaseapp.com",
+  projectId: "the-news-dragon-client-b5551",
+  storageBucket: "the-news-dragon-client-b5551.appspot.com",
+  messagingSenderId: "998372153542",
+  appId: "1:998372153542:web:b4ee95d96abcbcee211fc7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
