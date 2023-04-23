5d5c7f07fd72283595f325c7e22f4115dde83653
A Z M Monir Hossain <monir@AZMs-MacBook-Pro.local>
2023-04-23 22:47:08 +0600

    header

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import logo from "../../assets/logo.png";
 
 const Header = () => {
   return (
     <div>
-      <h2>This is Header</h2>
+      <img src={logo} alt="" />
+      <p className="text-secondary">
+        <small>Journalism Without Fear or Favour</small>
+      </p>
     </div>
   );
 };
