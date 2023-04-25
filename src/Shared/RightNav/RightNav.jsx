0da89a12369ac35a845104ef94b89bd6d9de2d2f
A Z M Monir Hossain <94215698+Monir-interact@users.noreply.github.com>
2023-04-25 19:35:31 +0600

    set find on

--- src/Shared/RightNav/RightNav.jsx
+++ src/Shared/RightNav/RightNav.jsx
@@ -1,13 +1,27 @@
-import React from "react";
-import { Button } from "react-bootstrap";
-import { FaGoogle, FaGithub } from 'react-icons/fa';
+
+import { Button, ListGroup } from "react-bootstrap";
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const RightNav = () => {
   return (
     <div>
       <h4>Login With</h4>
-      <Button variant="outline-primary"> <FaGoogle /> Login with Google</Button>
-      <Button variant="outline-secondary"> <FaGithub/> Login with GitHub</Button>
+      <Button variant="outline-primary">
+        {" "}
+        <FaGoogle /> Login with Google
+      </Button>
+      <Button variant="outline-secondary">
+        {" "}
+        <FaGithub /> Login with GitHub
+      </Button>
+      <div>
+        <h4>Find Us on</h4>
+        <ListGroup>
+          <ListGroup.Item> <FaFacebook/>  Facebook</ListGroup.Item>
+          <ListGroup.Item> <FaTwitter/> Twitter</ListGroup.Item>
+          <ListGroup.Item> <FaInstagram/> Instagram</ListGroup.Item>
+        </ListGroup>
+      </div>
     </div>
   );
 };
