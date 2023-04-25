1c652ab8d018aabf2db292890b0e2b272f3f61d0
A Z M Monir Hossain <94215698+Monir-interact@users.noreply.github.com>
2023-04-26 00:14:25 +0600

    set server site

--- src/Shared/Ad/Ad.jsx
+++ src/Shared/Ad/Ad.jsx
@@ -4,16 +4,18 @@ import { Figure } from "react-bootstrap";
 
 const Ad = () => {
   return (
-    <div className="text-center position-relative">
-      <Figure className="postion-relative">
-        <img src={bg} alt="" />
+    <div className="text-center position-relative z-index">
+      <div className="">
         <h4>Create an Amazing Newspaper</h4>
         <p>
           Discover thousands of options, easy to customize layouts, one-click to
           import demo and much more.
         </p>
         <button className="btn btn-danger">Learn More</button>
-      </Figure>
+      </div>
+      <div className="">
+        <img src={bg} alt="" />
+      </div>
     </div>
   );
 };
