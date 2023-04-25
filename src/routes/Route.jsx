a0ec93f0b08fae9091e55d5296aeb7e4257b2ad3
A Z M Monir Hossain <94215698+Monir-interact@users.noreply.github.com>
2023-04-26 00:55:48 +0600

    set outlet

--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/Home/Home/Home";
+import Category from "../pages/Home/Category/Category";
 
 const route = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const route = createBrowserRouter([
         path: "/",
         element: <Home />,
       },
+      {
+        path:'/category/:id',
+        element: <Category />,
+      }
     ],
   },
 ]);
