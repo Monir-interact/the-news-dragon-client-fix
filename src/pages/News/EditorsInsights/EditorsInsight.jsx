d638c766354c27ec8c308cfccc92104551d7606d
A Z M Monir Hossain <94215698+Monir-interact@users.noreply.github.com>
2023-04-26 12:05:19 +0600

    set editors insight

--- src/pages/News/EditorsInsights/EditorsInsight.jsx
+++ src/pages/News/EditorsInsights/EditorsInsight.jsx
@@ -1,52 +1,57 @@
 import React from "react";
 import { Card, Col, Row } from "react-bootstrap";
-import first from '../../../assets/1.png';
-import second from '../../../assets/2.png';
-import third from '../../../assets/3.png';
+import first from "../../../assets/1.png";
+import second from "../../../assets/2.png";
+import third from "../../../assets/3.png";
 
 const EditorsInsight = () => {
   return (
-    <Row xs={1} md={2} lg={3} className="g-4">
-      <Col>
-        <Card>
-          <Card.Img variant="top" src={first} />
-          <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </Col>
-      <Col>
-        <Card>
-          <Card.Img variant="top" src={second} />
-          <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </Col>
-      <Col>
-        <Card>
-          <Card.Img variant="top" src={third} />
-          <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
+    <div className="my-4">
+      <div>
+        <h4>Editors Insight</h4>
+      </div>
+      <Row xs={1} md={2} lg={3} className="g-4">
+        <Col>
+          <Card>
+            <Card.Img variant="top" src={first} />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Img variant="top" src={second} />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Img variant="top" src={third} />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
